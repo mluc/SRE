d7ee5398dde679e1b99ec0ae7e9ee3504cd6d60c
mluc <my.luc@opcity.com>
2023-01-09 19:41:35 -0600

    Add tf S3 and aws_ami

--- starter/terraform/_config.tf
+++ starter/terraform/_config.tf
@@ -1,8 +1,8 @@
 terraform {
    backend "s3" {
-     bucket = "udacity-terraform-course1"
+     bucket = "mluc-sre-terraform"
      key    = "terraform/terraform.tfstate"
-     region = "us-east-2"
+     region = "us-east-1"
    }
  }
 
